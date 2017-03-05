947f3c1e858ebb856033e126bf4f0ccb8480d7bc
Sty BL <stysdaman@gmail.com>
2017-03-05 16:28:49 +0200

    Implemented custom serialization options.

---
+++ DistCommon/Serialization/DistCommon.Serialization.CustomSettings.cs
@@ -0,0 +1,15 @@
+﻿namespace DistCommon.Serialization
+{
+    using System;
+    using Newtonsoft.Json;
+
+    public sealed class CustomSettings : JsonSerializerSettings
+    {
+        public CustomSettings() : base()
+        {
+            Converters.Add(new IPAddressConverter());
+            Converters.Add(new IPEndPointConverter());
+            Converters.Add(new TypeConverter());
+        }
+    }
+}
