543e35a7f14b425ed0304060255de5b0239d5b37
Sty BL <stysdaman@gmail.com>
2017-03-24 15:54:55 +0200

    Added reports.

---
+++ DistCommon/Comm/Reports/DistCommon.Comm.Reports.Base.cs
@@ -0,0 +1,20 @@
+﻿namespace DistCommon.Comm.Reports
+{
+    using System;
+
+    public class Base
+    {
+        public Base()
+        {
+            this.ReportType = this.GetType();
+        }
+
+        [Newtonsoft.Json.JsonConstructor]
+        public Base(Type reportType)
+        {
+            this.ReportType = reportType;
+        }
+
+        public Type ReportType { get; private set; }
+    }
+}
---
+++ DistCommon/Comm/Reports/DistCommon.Comm.Reports.WorkerExited.cs
@@ -0,0 +1,12 @@
+﻿namespace DistCommon.Comm.Reports
+{
+    public sealed class WorkerExited : Base
+    {
+        public WorkerExited(int id) : base()
+        {
+            this.ID = id;
+        }
+
+        public int ID { get; private set; }
+    }
+}
