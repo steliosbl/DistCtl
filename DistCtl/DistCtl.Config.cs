3524da57307c39941172b6112f0f1bad1a9da75c
Sty BL <stysdaman@gmail.com>
2017-03-25 20:46:22 +0200

    Added EnableRejectTooManyAssignments option to config.

--- DistCtl/DistCtl.Config.cs
+++ DistCtl/DistCtl.Config.cs
@@ -8,13 +8,14 @@
         public readonly bool EnableRedundancy;
         public readonly bool EnableLoadBalancing;
         public readonly bool EnableAutoRestart;
+        public readonly bool EnableRejectTooManyAssignments;
         public readonly int UpdateDelay;
         public readonly int TimeoutDuration;
         public readonly string SchematicFilename;
         public readonly string PreLoadFilename;
 
         [Newtonsoft.Json.JsonConstructor]
-        public Config(bool enableLiveErrors, bool enableLocalConsole, bool enableJobPreload, bool enableRedundancy, bool enableLoadBalancng, bool enableAutoRestart, int updateDelay, int timeoutDuration, string schematicFilename, string preLoadFilename)
+        public Config(bool enableLiveErrors, bool enableLocalConsole, bool enableJobPreload, bool enableRedundancy, bool enableLoadBalancng, bool enableAutoRestart, bool enableRejectTooManyAssignments, int updateDelay, int timeoutDuration, string schematicFilename, string preLoadFilename)
         {
             this.EnableLiveErrors = enableLiveErrors;
             this.EnableLocalConsole = enableLocalConsole;
@@ -22,6 +23,7 @@
             this.EnableRedundancy = enableRedundancy;
             this.EnableLoadBalancing = enableLoadBalancng;
             this.EnableAutoRestart = enableAutoRestart;
+            this.EnableRejectTooManyAssignments = enableRejectTooManyAssignments;
             this.UpdateDelay = updateDelay;
             this.TimeoutDuration = timeoutDuration;
             this.SchematicFilename = schematicFilename;
