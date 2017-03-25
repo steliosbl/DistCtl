bde00830ded624ccd37ade22b605e152e55d45d4
Sty BL <stysdaman@gmail.com>
2017-03-25 19:25:34 +0200

    Added job blueprint priority field.

--- DistCommon/Job/DistCommon.Job.Blueprint.cs
+++ DistCommon/Job/DistCommon.Job.Blueprint.cs
@@ -3,11 +3,13 @@
     public sealed class Blueprint
     {
         public readonly int ID;
+        public readonly int Priority;
         public readonly string Command;
         
-        public Blueprint(int id, string command)
+        public Blueprint(int id, int priority, string command)
         {
             this.ID = id;
+            this.Priority = priority;
             this.Command = command;
         }
     }
