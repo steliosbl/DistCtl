35af8c10405bc8101b7aabfb62c87405681e3d60
Sty BL <stysdaman@gmail.com>
2017-03-26 15:50:03 +0300

    Implemented Ctl runtime.

---
+++ DistCtlRuntime/DistCtlRuntime.Execute.cs
@@ -0,0 +1,19 @@
+﻿namespace DistCtlRuntime
+{
+    public static class Execute
+    {
+        [System.STAThread]
+        public static void Main(string[] args)
+        {
+            Newtonsoft.Json.JsonConvert.DefaultSettings = () => new DistCommon.Serialization.CustomSettings();
+            if (args.Length == 0)
+            {
+                new DistCtl.Controller();
+            }
+            else
+            {
+                new DistCtl.Controller(args[0]);
+            }
+        }
+    }
+}
