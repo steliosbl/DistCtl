9c49764de0ef5e7f22d65d066a478492eb907305
Sty BL <stysdaman@gmail.com>
2017-05-11 00:37:09 +0300

    Added RequiredFromQuery.

---
+++ DistCtlApi/DistCtlApi.RequiredFromQueryActionConstraint.cs
@@ -0,0 +1,27 @@
+﻿namespace DistCtlApi
+{
+    using System;
+    using Microsoft.AspNetCore.Mvc.ActionConstraints;
+
+    internal sealed class RequiredFromQueryActionConstraint : IActionConstraint
+    {
+        private readonly string parameter;
+
+        public RequiredFromQueryActionConstraint(string parameter)
+        {
+            this.parameter = parameter;
+        }
+
+        public int Order => 999;
+
+        public bool Accept(ActionConstraintContext context)
+        {
+            if (!context.RouteContext.HttpContext.Request.Query.ContainsKey(this.parameter))
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ DistCtlApi/DistCtlApi.RequiredFromQueryAttribute.cs
@@ -0,0 +1,18 @@
+﻿namespace DistCtlApi
+{
+    using System;
+    using System.Linq;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.AspNetCore.Mvc.ApplicationModels;
+
+    internal sealed class RequiredFromQueryAttribute : FromQueryAttribute, IParameterModelConvention
+    {
+        public void Apply(ParameterModel parameter)
+        {
+            if (parameter.Action.Selectors != null && parameter.Action.Selectors.Any())
+            {
+                parameter.Action.Selectors.Last().ActionConstraints.Add(new RequiredFromQueryActionConstraint(parameter.BindingInfo?.BinderModelName ?? parameter.ParameterName));
+            }
+        }
+    }
+}
