640c7a8026dff1aa8558d266d87cb17844a4409f
Sty BL <stysdaman@gmail.com>
2017-05-20 00:19:43 +0300

    Added logger severity enum.

---
+++ DistCommon/Logging/DistCommon.Logging.Severity.cs
@@ -0,0 +1,47 @@
+﻿namespace DistCommon.Logging
+{
+    public enum Severity
+    {
+        Info,
+        Warn,
+        Severe,
+        Critical
+    }
+
+    public static class SeverityInfo
+    {
+        public static string GetTag(this Severity severity)
+        {
+            switch (severity)
+            {
+                case Severity.Info:
+                    return "INFO";
+                case Severity.Warn:
+                    return "WARN";
+                case Severity.Severe:
+                    return "SEVERE";
+                case Severity.Critical:
+                    return "CRITICAL";
+                default:
+                    return "UNKNOWN";
+            }
+        }
+
+        public static System.ConsoleColor GetColor(this Severity severity)
+        {
+            switch (severity)
+            {
+                case Severity.Info:
+                    return System.ConsoleColor.White;
+                case Severity.Warn:
+                    return System.ConsoleColor.Yellow;
+                case Severity.Severe:
+                    return System.ConsoleColor.Red;
+                case Severity.Critical:
+                    return System.ConsoleColor.DarkRed;
+                default:
+                    return System.ConsoleColor.Magenta;
+            }
+        }
+    }
+}
