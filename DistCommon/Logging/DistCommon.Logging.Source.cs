98dbc1fbd549a06a3fd61660f884f1e019820b4f
Sty BL <stysdaman@gmail.com>
2017-05-20 00:19:54 +0300

    Added logger source enum.

---
+++ DistCommon/Logging/DistCommon.Logging.Source.cs
@@ -0,0 +1,33 @@
+﻿namespace DistCommon.Logging
+{
+    public enum Source
+    {
+        Ctl,
+        Console,
+        API,
+        Node,
+        Runtime
+    }
+
+    public static class SourceInfo
+    {
+        public static string GetName(this Source src)
+        {
+            switch (src)
+            {
+                case Source.Ctl:
+                    return "Ctl";
+                case Source.Console:
+                    return "Console";
+                case Source.API:
+                    return "API";
+                case Source.Node:
+                    return "Node";
+                case Source.Runtime:
+                    return "Bootstrap";
+                default:
+                    return "Unknown";
+            }
+        }
+    }
+}
